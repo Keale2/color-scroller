3b7c894edd85229d4b67ddbe00095426b4dc58e1
Kyle Wagner <Keale2@gmail.com>
2016-01-29 15:47:41 -0500

    Updates packages and lints with eslint

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,20 @@
 (function () {
 
-  document.addEventListener("DOMContentLoaded", function () {
-    update();
-  });
+    document.addEventListener("DOMContentLoaded", function () {
+        update();
+    });
 
-  function hsla(h=0, s=100, l=50, a=1) {
-    return `hsla(${h}, ${s}%, ${l}%, ${a})`;
-  }
+    function hsla(h = 0, s = 100, l = 50, a = 1) {
+        return `hsla(${h}, ${s}%, ${l}%, ${a})`;
+    }
 
-  function update() {
-    requestAnimationFrame(update);
-    document.body.style.backgroundColor = findColor(document.documentElement.scrollTop);
-  }
-  
-  function findColor(scroll) {
-    var quotient, remainder;
-    quotient = Math.floor(scroll / 360);
-    remainder = scroll / 10 % 360;
-    
-    return hsla(remainder);
-  }
-  
+    function update() {
+        requestAnimationFrame(update);
+        document.body.style.backgroundColor = findColor(document.documentElement.scrollTop);
+    }
+
+    function findColor(scroll) {
+        return hsla(scroll / 10 % 360);
+    }
+
 })();
--- gruntfile.js
+++ gruntfile.js
@@ -1,141 +1,135 @@
 "use strict";
 module.exports = function (grunt) {
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    babel: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        src: 'dist/js/main.js',
-        dest: 'dist/js/main.js'
-      }
-    },
-
-    sass: {
-      dev: {
-        options: {
-          sourceMap: true
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ["es2015"]
+            },
+            dist: {
+                src: "dist/js/main.js",
+                dest: "dist/js/main.js"
+            }
         },
-        src: 'app/scss/main.scss',
-        dest: 'dist/css/main.css'
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed',
-          sourceMap: true
+
+        sass: {
+            dev: {
+                options: {
+                    sourceMap: true
+                },
+                src: "app/scss/main.scss",
+                dest: "dist/css/main.css"
+            },
+            dist: {
+                options: {
+                    outputStyle: "compressed",
+                    sourceMap: true
+                },
+                src: "app/scss/main.scss",
+                dest: "dist/css/main.min.css"
+            }
         },
-        src: 'app/scss/main.scss',
-        dest: 'dist/css/main.min.css'
-      }
-    },
 
-    concat: {
-      dev: {
-        options: {
-          sourceMap: true
+        concat: {
+            dev: {
+                options: {
+                    sourceMap: true
+                },
+                src: ["app/js/**/*.js"],
+                dest: "dist/js/main.js"
+            }
         },
-        src: ['app/js/**/*.js'],
-        dest: 'dist/js/main.js'
-      }
-    },
 
-    watch: {
-      scss: {
-        files: 'app/scss/**/*.scss',
-        tasks: ['sass:dev', 'autoprefixer:dev', 'captain_hook:dev']
-      },
-      js: {
-        files: 'app/js/**/*.js',
-        tasks: ['jshint', 'concat', 'babel', 'captain_hook:dev']
-      },
-      html: {
-        files: 'app/**/*.html',
-        tasks: ['copy:html', 'captain_hook:dev']
-      }
-    },
-
-    browserSync: {
-      bsFiles: {
-        src: 'dist/**/*'
-      },
-      options: {
-        server: 'dist',
-        watchTask: true
-      }
-    },
-
-    uglify: {
-      deploy: {
-        src: 'dist/js/main.js',
-        dest: 'dist/js/main.min.js'
-      }
-    },
-
-    autoprefixer: {
-      options: {
-        browsers: ['last 3 versions']
-      },
-      dev: {
-        src: 'dist/css/main.css',
-        dest: 'dist/css/main.css'
-      },
-      dist: {
-        src: 'dist/css/main.min.css',
-        dest: 'dist/css/main.min.css'
-      }
-    },
-
-    copy: {
-      html: {
-        cwd: 'app/',
-        src: '**/*.html',
-        dest: 'dist/',
-        expand: true
-      },
-      fonts: {
-        cwd: 'app/',
-        src: 'fonts/**/*',
-        dest: 'dist/',
-        expand: true
-      }
-    },
-
-    captain_hook: {
-      dev: {
-        options: {
-          cwd: "dist"
+        watch: {
+            scss: {
+                files: "app/scss/**/*.scss",
+                tasks: ["sass:dev", "autoprefixer:dev", "captain_hook:dev"]
+            },
+            js: {
+                files: "app/js/**/*.js",
+                tasks: ["concat", "babel", "captain_hook:dev"]
+            },
+            html: {
+                files: "app/**/*.html",
+                tasks: ["copy:html", "captain_hook:dev"]
+            }
         },
-        jsFiles: ['js/**/*.js', '!js/**/*.min.js'],
-        cssFiles: ['css/**/*.css', '!css/**/*.min.css'],
-        targetHtml: ['index.html']
-      },
-      dist: {
-        options: {
-          cwd: "dist"
+
+        browserSync: {
+            bsFiles: {
+                src: "dist/**/*"
+            },
+            options: {
+                server: "dist",
+                watchTask: true
+            }
         },
-        jsFiles: ['js/**/*.min.js'],
-        cssFiles: ['css/**/*.min.css'],
-        targetHtml: ['index.html']
-      }
-    },
 
-    jshint: {
-      options: {
-        jshintrc: true
-      },
-      all: ['Gruntfile.js', 'app/js/**/*.js']
-    },
+        uglify: {
+            deploy: {
+                src: "dist/js/main.js",
+                dest: "dist/js/main.min.js"
+            }
+        },
 
-    clean: ['dist']
+        autoprefixer: {
+            options: {
+                browsers: ["last 3 versions"]
+            },
+            dev: {
+                src: "dist/css/main.css",
+                dest: "dist/css/main.css"
+            },
+            dist: {
+                src: "dist/css/main.min.css",
+                dest: "dist/css/main.min.css"
+            }
+        },
 
-  });
+        copy: {
+            html: {
+                cwd: "app/",
+                src: "**/*.html",
+                dest: "dist/",
+                expand: true
+            },
+            fonts: {
+                cwd: "app/",
+                src: "fonts/**/*",
+                dest: "dist/",
+                expand: true
+            }
+        },
 
-  require('load-grunt-tasks')(grunt);
+        captain_hook: {
+            dev: {
+                options: {
+                    cwd: "dist"
+                },
+                jsFiles: ["js/**/*.js", "!js/**/*.min.js"],
+                cssFiles: ["css/**/*.css", "!css/**/*.min.css"],
+                targetHtml: ["index.html"]
+            },
+            dist: {
+                options: {
+                    cwd: "dist"
+                },
+                jsFiles: ["js/**/*.min.js"],
+                cssFiles: ["css/**/*.min.css"],
+                targetHtml: ["index.html"]
+            }
+        },
 
-  grunt.registerTask('default', ['clean', 'copy', 'sass:dev', 'jshint', 'concat', 'babel', 'autoprefixer:dev', 'captain_hook:dev', 'browserSync', 'watch']);
-  grunt.registerTask('dist', ['clean', 'copy', 'sass:dist', 'jshint', 'concat', 'babel', 'autoprefixer:dist', 'uglify', 'captain_hook:dist']);
+        clean: ["dist"]
+
+    });
+
+    require("load-grunt-tasks")(grunt);
+
+    grunt.registerTask("default", ["clean", "copy", "sass:dev", "concat", "babel", "autoprefixer:dev", "captain_hook:dev", "browserSync", "watch"]);
+    grunt.registerTask("dist", ["clean", "copy", "sass:dist", "concat", "babel", "autoprefixer:dist", "uglify", "captain_hook:dist"]);
 
 };
 
